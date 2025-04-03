1db91d2b5e1e13e31b7b4ac20040ac25e84a8ce8
Andrea Bonvissuto <abonvissuto@deloitte.it>
2025-04-03 17:09:02 +0200

    Migrated init to s3 backend

--- init_aws/storage_conf.tf
+++ init_aws/storage_conf.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket              = "terraform-storage-config"
+  bucket              = var.default_config_bucket
   object_lock_enabled = true
-  force_destroy = true # allows to destroy the bucket even if contains files 
+  force_destroy       = true # allows to destroy the bucket even if contains files 
 }
